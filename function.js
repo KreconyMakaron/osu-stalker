e3acfc82483f92a5528db879e4f7ea1519966150
KreconyMakaron <mpawlowski795@gmail.com>
2023-12-16 22:25:57 +0100

    hih

--- function.js
+++ function.js
@@ -1,3 +1,15 @@
 import puppeteer from "puppeteer"
 
+const getQuotes = async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    defaultViewport: null,
+  });
 
+  const page = await browser.newPage();
+  await page.goto("http://quotes.toscrape.com/", {
+    waitUntil: "domcontentloaded",
+  });
+};
+
+console.log(getQuotes());
