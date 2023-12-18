6cbdd801aa314492de84aa8f50f84e5dfc6c8f68
KreconyMakaron <mpawlowski795@gmail.com>
2023-12-18 17:38:44 +0100

    restarcik lekki

---
+++ function.js
@@ -0,0 +1,12 @@
+fetch('https://osu.ppy.sh')
+    .then(function(response) {
+        return response.text()
+    })
+    .then(function(html) {
+        var parser = new DOMParser();
+        var doc = parser.parseFromString(html, "text/html");
+        console.log(doc);
+    })
+    .catch(function(err) {  
+        console.log('Failed to fetch page: ', err);  
+    });
